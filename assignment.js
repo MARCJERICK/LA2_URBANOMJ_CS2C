6fd17967cc2b325adf8724b12fdda94aa6502069
Marc Jerick <umarcjerick@gmail.com>
2024-09-22 18:47:56 +0800

    initial commit with app.js

---
+++ assignment.js
@@ -0,0 +1,84 @@
+// Declaring the student's full name and appending "was born"
+let firstname = "Marc Jerick Marbel Urbano ";
+firstname += "was born ";
+
+// Declaring the student's birth date and appending "at"
+let birthdate = "March 10, 2005 ";
+birthdate += "at ";
+
+// Declaring the student's place of birth and appending "and currently living at"
+let birthplace = "Bagani Tocgo, Candon City, Ilocos Sur, 2710 ";
+birthplace += "and currently living at ";
+
+// Declaring the student's current address
+let address = "Paras, Candon City, Ilocos Sur, 2710. ";
+
+// Adding the course the student is taking and appending the year/section
+address += "Marc Jerick Marbel Urbano is taking up ";
+let courseyear = "Bachelor of Science in Computer Science, 2nd year ";
+
+// Declaring the student's dream job after graduation
+courseyear += "and dream to become ";
+let Dreamjob = "full stack developer ";
+Dreamjob += "after graduation.";
+
+// Concatenating all the strings to form a full sentence
+let sentence = firstname + birthdate + birthplace + address + courseyear + Dreamjob;
+
+// Declaring the student 2 full name and appending "was born"
+let firstname2 = "Joshua Morales Toquero ";
+firstname2 += "was born ";
+
+// Declaring the student's birth date and appending "at"
+let birthdate2 = "September 25, 2004 ";
+birthdate2 += "at ";
+
+// Declaring the student's place of birth and appending "and currently living at"
+let birthplace2 = "Pio, Porac Pampanga Philippines, 2008 ";
+birthplace2 += "and currently living at ";
+
+// Declaring the student's current address
+let address2 = "Patpata Segundo, Candon City, Ilocos Sur, 2710. ";
+
+// Adding the course the student is taking and appending the year/section
+address2 += "Joshua Morales Toquero is taking up ";
+let courseyear2 = "Bachelor of Science in Computer Science, 2nd year ";
+
+// Declaring the student's dream job after graduation
+courseyear2 += "and dream to become ";
+let Dreamjob2 = "full stack web developer ";
+Dreamjob2 += "after graduation.";
+
+// Concatenating all the strings to form a full sentence
+let sentence2 = firstname2 + birthdate2 + birthplace2 + address2 + courseyear2 + Dreamjob2;
+
+// Declaring the student 2 full name and appending "was born"
+let firstname3 = "Rolly Mae Morales Caay ";
+firstname3 += "was born ";
+
+// Declaring the student's birth date and appending "at"
+let birthdate3 = "April 11, 2005 ";
+birthdate3 += "at ";
+
+// Declaring the student's place of birth and appending "and currently living at"
+let birthplace3 = "Sitio Baliw, Villa Hermosa, Sta Cruz Ilocos Sur, 2713 ";
+birthplace3 += "and currently living at ";
+
+// Declaring the student's current address
+let address3 = "Sitio Baliw, Villa Hermosa, Sta Cruz Ilocos Sur, 2713. ";
+
+// Adding the course the student is taking and appending the year/section
+address3 += "Rolly Mae Morales Caay is taking up ";
+let courseyear3 = "Bachelor of Science in Computer Science, 2nd year ";
+
+// Declaring the student's dream job after graduation
+courseyear3 += "and dream to become ";
+let Dreamjob3 = "Cybersecurity ";
+Dreamjob3 += "after graduation.";
+
+// Concatenating all the strings to form a full sentence
+let sentence3 = firstname3 + birthdate3 + birthplace3 + address3 + courseyear3 + Dreamjob3;
+
+
+// Logging the final sentence to the console and line break
+console.log(sentence + "\n \n" + sentence2 + "\n \n" + sentence3);
